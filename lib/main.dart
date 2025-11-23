8753de81a130543ab4a0689d7395c23d7ab5b7ce
Thanakorn Amnuaywiboolpol <thanakorn.benjamin@gmail.com>
2025-11-23 14:18:24 +0700

    feat: clone from first_app

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:first_app/gradient_container.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(Colors.lightBlueAccent, Colors.blueAccent),
+      ),
+    ),
+  );
+}
