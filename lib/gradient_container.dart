5876e408c29ecf0aee89421c2990b56b62d51dc1
Thanakorn Amnuaywiboolpol <thanakorn.benjamin@gmail.com>
2025-11-23 14:43:08 +0700

    feat: Add "Roll Dice" button and `rollDice` method to `GradientContainer` for interactive dice rolling.

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -13,6 +13,8 @@ class GradientContainer extends StatelessWidget {
   final Color color1;
   final Color color2;
 
+  void rollDice() {}
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -23,7 +25,24 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: Center(child: Image.asset("assets/images/dice-1.png", width: 200)),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset("assets/images/dice-1.png", width: 200),
+            const SizedBox(height: 20),
+            TextButton(
+              onPressed: rollDice,
+              style: TextButton.styleFrom(
+                // padding: const EdgeInsets.only(top: 20),
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(fontSize: 28),
+              ),
+              child: const Text("Roll Dice"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
